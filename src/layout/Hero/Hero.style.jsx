dea302fed3e84ba1e1fb023bfea7356a407f93b6
dominggo1999 <arnolddominggo19@gmail.com>
2022-05-25 19:41:00 +0700

    add demo video file

--- src/layout/Hero/Hero.style.jsx
+++ src/layout/Hero/Hero.style.jsx
@@ -52,13 +52,17 @@ export const DemoVideo = styled.div`
   ${tw`
     w-full 
     xl:w-2/3
-    xl:min-h-[460px]
+    xl:min-h-[460px] 
   `}
+
+  
 
   .plyr{
     ${tw`
       w-full
     `}
+
+    box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 
     --plyr-color-main : ${theme`colors.accent`} ;
   }
